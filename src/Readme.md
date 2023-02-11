c140e3b9e1a1ae2aa24492cde5047f5f4c033668
shreya kumari <shreypilot28@gmail.com>
2023-02-11 14:55:18 +0530

    session 6 assignment

---
+++ src/Readme.md
@@ -0,0 +1,22 @@
+## **Namaste React Course by Akshay Saini**
+
+# **Chapter 06 - Exploring the world**
+
+### **Coding Assignment :**
+
+- Play with the useEffect Hook to see when it is called? (before or after render)
+- Play with the dependency array in useEffect Hook.
+- Play with the developer console by putting a debugger in render and useEffect.
+- Call an actual API call.
+- Handle Error in your API call.
+- Build Shimmer UI when data is not loaded.
+- Render your UI with actual API data.
+- Make Search functionality work.
+- Make a Login Logout button that  toggles with a state.
+>  Note: Please enable CORS on your browser before accessing the FoodFire application. Download the Allow CORS chrome extension. 
+
+
+
+
+### **Live Project App Link**
+ Open [an Apni Rasoi App from scratch using React.js"](http://localhost:3002) to view it in your browser.
---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Rasoi from "./Images/Rasoi.jpg";
+import "./Header.css";
+
+// Title component for display logo
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src={Rasoi}
+      alt="Rasoi logo"
+      title="Apni Rasoi"
+    />
+  </a>
+);
+
+// Header component for header section: Logo, Nav Items
+const Header = () => {
+  // use useState for user logged in or logged out
+  const [isLoggedin, setIsLoggedin] = useState(true);
+
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+          <li>
+            {/* use conditional rendering for login and logout */}
+            {isLoggedin ? (
+              <button
+                className="logout-btn"
+                onClick={() => setIsLoggedin(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button className="login-btn" onClick={() => setIsLoggedin(true)}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
