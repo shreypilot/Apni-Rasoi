9ddeab9c7a316e1f849eef5ee1c2c229b10cdba1
shreya kumar <shreypilot28@gmail.com>
2023-04-03 12:49:58 +0530

    cart item problem solved

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -15,7 +15,7 @@ const Cart = () => {
     <div>
       <h1 className="font-bold text-3xl"> Cart Items - {cartItems.length}</h1>
       <button
-        className="bg-green-100 p-2 m-5"
+        className="bg-red-600 p-2 m-5 font-bold mt-2 uppercase"
         onClick={() => handleClearCart()}
       >
         Clear Cart
--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,13 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { IMG_CDN_URL } from "../constants";
+import { ITEM_IMG_CDN_URL } from "../constants";
+import  {removeItem} from "../utils/CartSlice";
+import { useDispatch } from "react-redux";
 
-const FoodItem = ({ name, description, cloudinaryImageId, price }) => {
+const FoodItem = ({ name, description, imageId, price }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveItem = () => {
+    dispatch(removeItem());
+  };
+
   return (
-    <div className="w-56 p-2 m-2 shadow-lg bg-pink-50">
-      <img src={IMG_CDN_URL + cloudinaryImageId} />
-      <h2 className="font-bold text-xl">{name}</h2>
-      <h3>{description}</h3>
-      <h4>Rupees: {price / 100}</h4>
+    <div className="w-56 p-2 m-4 shadow-lg bg-pink-200">
+      <img
+          className="rounded-md md:w-28 item-center m-4"
+          src={ITEM_IMG_CDN_URL + imageId}
+          alt={name}
+        />
+      <h2 className="font-bold text-xl m-4">{name}</h2>
+      <h3 className="m-4">{description}</h3>
+      <h4 className="m-4">Rupees: {price / 100}</h4>
+      <button
+        className="bg-green-400 p-3 ml-4 text-bold uppercase"
+        onClick={() => handleRemoveItem()}
+      >
+        Remove
+      </button>
     </div>
   );
 };
